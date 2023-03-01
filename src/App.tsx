057f88062b3f8f3829cec6ee69a8e455cfb3dc2c
a-zyk <austejazykute@gmail.com>
2023-03-01 17:46:20 +0200

    Implement sort, fix set to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,19 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Text from "./Text";
+import ToDoList from "./ToDoList";
 
-const deleteIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-    />
-  </svg>
-);
-
-const doneIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-);
+interface toDoItem {
+  text: string;
+  done: boolean;
+}
 
 const App: React.FC = () => {
-  const [toDoList, setToDoList] = useState<{ text: string; done: boolean }[]>(
-    []
-  );
+  const [toDoList, setToDoList] = useState<toDoItem[]>([]);
 
   useEffect(() => {
     const localToDos = localStorage.getItem("toDos");
-    const result = localToDos ? JSON.parse(localToDos) : undefined;
+    const result = localToDos ? JSON.parse(localToDos) : [];
     setToDoList(result);
   }, []);
 
@@ -52,6 +22,7 @@ const App: React.FC = () => {
       return index !== i;
     });
     setToDoList(newToDoList);
+    localStorage.setItem("toDos", JSON.stringify(newToDoList));
   };
 
   const onDone = (i: number) => {
@@ -61,26 +32,23 @@ const App: React.FC = () => {
       }
       return item;
     });
+
     setToDoList(doneToTrue);
+    localStorage.setItem("toDos", JSON.stringify(doneToTrue));
+  };
+
+  const onNewToDo = (toDo: toDoItem) => {
+    setToDoList((previousToDos: toDoItem[]) => {
+      const newToDos = [toDo, ...previousToDos];
+      localStorage.setItem("toDos", JSON.stringify(newToDos));
+      return newToDos;
+    });
   };
 
   return (
     <div>
-      <Text setToDoList={setToDoList} />
-      <div>
-        {toDoList.map((item, i) => (
-          <div
-            className="flex items-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-            key={i}
-          >
-            <span>{item.text}</span>
-            <div className="flex flex-grow"></div>
-            <div>{item.done ? "y" : "n"}</div>
-            <button onClick={() => onDone(i)}>{doneIcon}</button>
-            <button onClick={() => onDelete(i)}>{deleteIcon}</button>
-          </div>
-        ))}
-      </div>
+      <Text onNewToDo={onNewToDo} />
+      <ToDoList onDone={onDone} onDelete={onDelete} toDoList={toDoList} />
     </div>
   );
 };
--- src/Text.tsx
+++ src/Text.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const TextInput: React.FC<{ setToDoList: any }> = ({ setToDoList }) => {
+const TextInput: React.FC<{ onNewToDo: any }> = ({ onNewToDo }) => {
   const [toDoText, setToDoText] = useState<string>("");
   const handleToDoSubmit = () => {
     if (!toDoText.length) return;
@@ -8,29 +8,29 @@ const TextInput: React.FC<{ setToDoList: any }> = ({ setToDoList }) => {
       text: toDoText,
       done: false,
     };
-    setToDoList((previousToDos: { text: string; done: boolean}[]) => {
-      const newToDos = [toDo, ...previousToDos];
-      localStorage.setItem("toDos", JSON.stringify(newToDos))
-      return newToDos
-    });
+    onNewToDo(toDo);
+
     setToDoText("");
   };
 
   return (
-    <div className="flex gap-3 items-center justify-center mt-5">
-      <input
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/5 p-2.5 "
-        value={toDoText}
-        onChange={(e) => setToDoText(e.target.value)}
-        type="text"
-      ></input>
-      <button
-        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-        onClick={handleToDoSubmit}
-      >
-        Add
-      </button>
-    </div>
+    <>
+      <div className="text-3xl flex items-center justify-center mt-2 ">What do you want to do today?</div>
+      <div className="flex gap-3 items-center justify-center mt-5">
+        <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/5 p-2.5 "
+          value={toDoText}
+          onChange={(e) => setToDoText(e.target.value)}
+          type="text"
+        ></input>
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+          onClick={handleToDoSubmit}
+        >
+          Add
+        </button>
+      </div>
+    </>
   );
 };
 
